ede5f3beb1082937678f11bfba52708c69bfbcb8
Ömer Akkoyun <33864154+OmerAkkoyun@users.noreply.github.com>
2018-11-22 18:36:22 +0300

    c#

---
+++ Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Pc_Shutdown_Program
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(59, 307);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(306, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(11, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(401, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bilgisayar Ne Zaman Kapatılsın?";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft YaHei", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton1.Location = new System.Drawing.Point(47, 58);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(152, 31);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "1 saat sonra";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft YaHei", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton2.Location = new System.Drawing.Point(226, 58);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(152, 31);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "2 saat sonra";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft YaHei", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton3.Location = new System.Drawing.Point(47, 123);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(152, 31);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "3 saat sonra";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Microsoft YaHei", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton4.Location = new System.Drawing.Point(226, 123);
+            this.radioButton4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(152, 31);
+            this.radioButton4.TabIndex = 5;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "4 saat sonra";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Font = new System.Drawing.Font("Microsoft YaHei", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton5.Location = new System.Drawing.Point(47, 186);
+            this.radioButton5.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(152, 31);
+            this.radioButton5.TabIndex = 6;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "5 saat sonra";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Font = new System.Drawing.Font("Microsoft YaHei", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton6.Location = new System.Drawing.Point(226, 186);
+            this.radioButton6.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(152, 31);
+            this.radioButton6.TabIndex = 7;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "6 saat sonra";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Gray;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft YaHei", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(165, 237);
+            this.textBox1.MaxLength = 6;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 35);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DeepPink;
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(131, 374);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 41);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "İptal Et";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(59, 237);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 34);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Elle Gir";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 8.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Location = new System.Drawing.Point(157, 273);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Dakika cinsinden girin. 1saat=60";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.ClientSize = new System.Drawing.Size(422, 434);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximumSize = new System.Drawing.Size(438, 473);
+            this.MinimumSize = new System.Drawing.Size(438, 473);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Otomatik Bilgisayar Kapatma Programı-ÖA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//kütüphaneyi ekledik.
+using System.Diagnostics;
+
+namespace Pc_Shutdown_Program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //başlat button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi = 3600;
+            //radiobuton değerlerini alalım.
+
+            if (radioButton1.Checked == true)
+            {
+                sayi = 3600;
+                MessageBox.Show("Bilgisayarınız 1 saat sonra kapatılacaktır.");
+            }
+            else if (radioButton2.Checked)
+            {
+                sayi = 3600 * 2;
+                MessageBox.Show("Bilgisayarınız 2 saat sonra kapatılacaktır.");
+
+            }
+
+            else if (radioButton3.Checked == true)
+            {
+                sayi = 3600 * 3;
+                MessageBox.Show("Bilgisayarınız 3 saat sonra kapatılacaktır.");
+            }
+
+            else if (radioButton4.Checked == true)
+            {
+                sayi = 3600 * 4;
+                MessageBox.Show("Bilgisayarınız 4 saat sonra kapatılacaktır.");
+            }
+
+            else if (radioButton5.Checked == true)
+            {
+                sayi = 3600 * 5;
+                MessageBox.Show("Bilgisayarınız 5 saat sonra kapatılacaktır.");
+            }
+            else if (radioButton6.Checked == true)
+            {
+                sayi = 3600 * 6;
+                MessageBox.Show("Bilgisayarınız 6 saat sonra kapatılacaktır.");
+            }
+            else
+            {
+
+                if (textBox1.Text != "")
+                {
+                    try
+                    {
+                        textBox1.Enabled = true;
+                        int deger =( int.Parse(textBox1.Text));
+
+                        int saat=Convert.ToInt32(deger/60);//saat değeri aldık 1,4548458...
+
+                        double kalan =(deger%60); //modu aldık ki 65 girilirse kalan 5 dakika olacak
+                        double milisaniye = (deger*60);
+
+                       System.Diagnostics.Process.Start("CMD.exe", "/C shutdown -s -t " + milisaniye);
+
+                        MessageBox.Show("Bilgisayarınız " + deger + " dakika sonra kapatılacaktır.\n\n\nYani: " + saat + " saat " + kalan + " dakika sonra", " Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Lütfen Sadece Tam Sayı Girin !", "Uyarı",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                   
+
+                }
+
+                else
+                {
+                    textBox1.Text = "60";
+                    textBox1.Enabled = true;
+                
+                    MessageBox.Show("Bilgisayarınız 1 saat sonra kapatılacaktır.\n", " Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+            }
+
+            System.Diagnostics.Process.Start("CMD.exe", "/C shutdown -s -t " + sayi);
+
+
+
+            radioButton1.Enabled = false;
+            radioButton2.Enabled = false;
+            radioButton3.Enabled = false;
+            radioButton4.Enabled = false;
+            radioButton5.Enabled = false;
+            radioButton6.Enabled = false;
+            textBox1.Enabled = false;
+            button1.Enabled = false;
+            button3.Enabled = false;
+            button2.Enabled = true;
+
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void button2_Click(object sender, EventArgs e) //İPTAL ET BUTTON
+        {
+            System.Diagnostics.Process.Start("CMD.exe", "/C shutdown -a");
+            MessageBox.Show("Otomatik Bilgisayar Kapatma Sistemi\nDevre Dışı Bırakıldı!", " Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+            radioButton1.Enabled = true;
+            radioButton2.Enabled = true;
+            radioButton3.Enabled = true;
+            radioButton4.Enabled = true;
+            radioButton5.Enabled = true;
+            radioButton6.Enabled = true;
+            textBox1.Enabled = true;
+            button1.Enabled = true;
+            button3.Enabled = true;
+
+            /////
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+            radioButton4.Checked = false;
+            radioButton5.Checked = false;
+            radioButton6.Checked = false;
+            textBox1.Clear();
+
+            button2.Enabled = false;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)  //Elle Gir Button
+        {
+
+            radioButton1.Checked = false;//önce bunları false yaptık  ki elle gir butonuna  ilk tıklamada -
+            radioButton2.Checked = false;// radiobutonlar pasif olsun textbox enabled olabilsin...
+            radioButton3.Checked = false;
+            radioButton4.Checked = false;
+            radioButton5.Checked = false;
+            radioButton6.Checked = false;
+
+            textBox1.Enabled = true;
+
+            textBox1.BackColor = Color.White;
+            textBox1.Focus();
+            label2.Enabled = true;
+
+           
+
+        }
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+            textBox1.Enabled = false;
+            button2.Enabled = false;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+            label2.Visible = false;
+            textBox1.BackColor = Color.Gray;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+            label2.Visible = false;
+            textBox1.BackColor = Color.Gray;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+            label2.Visible = false;
+            textBox1.BackColor = Color.Gray;
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+            label2.Visible = false;
+            textBox1.BackColor = Color.Gray;
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+            label2.Visible = false;
+            textBox1.BackColor = Color.Gray;
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+            label2.Visible = false;
+            textBox1.BackColor = Color.Gray;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+    }
+}
